7a256dcd853806faf49d339bd3748c488af273d5
itphilip1998 <itkeyzzpianny@gmail.com>
2022-07-19 09:40:25 +0100

    dev

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PayPalController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,9 +16,3 @@ use App\Http\Controllers\PayPalController;
 Route::get('/', function () {
     return view('welcome');
 });
-
-
-Route::get('create-transaction', [PayPalController::class, 'createTransaction'])->name('createTransaction');
-Route::get('process-transaction', [PayPalController::class, 'processTransaction'])->name('processTransaction');
-Route::get('success-transaction', [PayPalController::class, 'successTransaction'])->name('successTransaction');
-Route::get('cancel-transaction', [PayPalController::class, 'cancelTransaction'])->name('cancelTransaction');
