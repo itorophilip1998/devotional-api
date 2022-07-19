bac9f4af2480f2bcbeb7d42b63d3310e915bb87b
itphilip1998 <itkeyzzpianny@gmail.com>
2022-07-19 11:26:32 +0100

    jwt fixed

--- config/database.php
+++ config/database.php
@@ -77,7 +77,20 @@ return [
             'search_path' => 'public',
             'sslmode' => 'prefer',
         ],
-           
+          'pgsql' => [
+            'driver' => 'pgsql',
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST'),
+            'port' => env('DB_PORT'),
+            'database' => env('DB_DATABASE'),
+            'username' => env('DB_USERNAME'),
+            'password' => env('DB_PASSWORD'),
+            'charset' => 'utf8',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'schema' => 'public',
+            'sslmode' => 'prefer',
+        ],
 
 
         'sqlsrv' => [
