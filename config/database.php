56e4491ace66135042713d1992be256e85e6b664
itphilip1998 <itkeyzzpianny@gmail.com>
2022-07-19 11:22:07 +0100

    jwt fixed

--- config/database.php
+++ config/database.php
@@ -68,15 +68,17 @@ return [
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST'),
             'port' => env('DB_PORT'),
-            'database' => env('DB_DATABASE'),
-            'username' => env('DB_USERNAME'),
-            'password' => env('DB_PASSWORD'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
             'search_path' => 'public',
             'sslmode' => 'prefer',
         ],
+           
+
 
         'sqlsrv' => [
             'driver' => 'sqlsrv',
